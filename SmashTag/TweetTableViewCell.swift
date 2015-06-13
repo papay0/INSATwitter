985169807add7e7aa02d8f4cc70d6c72b8810af6
papay0 <arth83@hotmail.fr>
2015-06-14 00:51:15 +0200

    add Date Label

--- SmashTag/TweetTableViewCell.swift
+++ SmashTag/TweetTableViewCell.swift
@@ -19,7 +19,9 @@ class TweetTableViewCell: UITableViewCell {
     @IBOutlet weak var tweetProfileImageView: UIImageView!
     @IBOutlet weak var tweetScreenNameLabel: UILabel!
     @IBOutlet weak var tweetTextLabel: UILabel!
-
+    @IBOutlet weak var tweetCreatedLabel: UILabel!
+    
+    
     func updateUI() {
         // reset any existing tweet information
         tweetTextLabel?.attributedText = nil
@@ -39,51 +41,32 @@ class TweetTableViewCell: UITableViewCell {
             
             tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
             
-            // TODO: Unblock the main thread !
+            
             if let profileImageURL = tweet.user.profileImageURL {
+                let colorForBorder = UIColor.blackColor()
                 let qos = Int(QOS_CLASS_USER_INITIATED.rawValue) // qos = quality of service (if it's slow, importante...)
                 dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
                     if let imageData = NSData(contentsOfURL: profileImageURL) {
                         dispatch_async(dispatch_get_main_queue(), {
-                                    self.tweetProfileImageView?.image = UIImage(data: imageData)
-                                    self.tweetProfileImageView?.layer.cornerRadius = self.tweetProfileImageView.frame.size.width / 2
-                                    self.tweetProfileImageView?.clipsToBounds = true
+                            self.tweetProfileImageView?.image = UIImage(data: imageData)
+                            //self.tweetProfileImageView?.layer.cornerRadius = self.tweetProfileImageView.frame.size.width / 2
+                            self.tweetProfileImageView?.layer.cornerRadius = 10.0
+                            self.tweetProfileImageView?.clipsToBounds = true
+                            self.tweetProfileImageView?.layer.borderWidth = 3.0
+                            self.tweetProfileImageView?.layer.borderColor = colorForBorder.CGColor
                             }
                         )
                     }
-                 }
-            }
-            
-            /*
-            if let url = imageURL{
-                spinner?.startAnimating()
-                let qos = Int(QOS_CLASS_USER_INITIATED.rawValue) // qos = quality of service (if it's slow, importante...)
-                dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
-                    let imageData = NSData(contentsOfURL: url)
-                    dispatch_async(dispatch_get_main_queue(), {
-                        if url == self.imageURL { // in order to check if I still ask for the same image
-                            if imageData != nil {
-                                self.image = UIImage(data: imageData!)
-                            } else {
-                                self.image = nil
-                            }
-                        }
-                    })
                 }
             }
-            */
-
-            
-            
-            
-//            
-//            let formatter = NSDateFormatter()
-//            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
-//                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
-//            } else {
-//                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
-//            }
-//            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+    
+           let formatter = NSDateFormatter()
+            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+            } else {
+                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            }
+            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
         }
         
     }
