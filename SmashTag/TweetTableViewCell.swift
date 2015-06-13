6e8ce258313e10dba9861fd1c2300f46017ce55d
papay0 <arth83@hotmail.fr>
2015-06-13 23:20:57 +0200

    add Twiter Like API

---
+++ SmashTag/TweetTableViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  TweetTableViewCell.swift
+//  SmashTag
+//
+//  Created by Arthur Papayo on 6/13/15.
+//  Copyright © 2015 Arthur Papayo. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    var tweet:Tweet?{
+        didSet{
+            updateUI()
+        }
+    }
+   
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+
+    func updateUI() {
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+//        tweetCreatedLabel?.text = nil
+        
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet
+        {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil  {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
+            
+            // TODO: Unblock the main thread !
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) { // blocks main thread!
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+//            
+//            let formatter = NSDateFormatter()
+//            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+//                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+//            } else {
+//                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+//            }
+//            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+        }
+        
+    }
+
+    
+}
---
+++ SmashTag/TweetTableViewController.swift
@@ -0,0 +1,141 @@
+//
+//  TweetTableViewController.swift
+//  SmashTag
+//
+//  Created by Arthur Papayo on 6/13/15.
+//  Copyright © 2015 Arthur Papayo. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    
+    var tweets = [[Tweet]]()
+    
+    var searchText : String? = "#INSA" {
+        didSet {
+            searchTextField?.text = searchText
+            tweets.removeAll()
+            tableView.reloadData()
+            refresh()
+        }
+    }
+    
+    func refresh(){
+        if searchText != nil {
+            let request = TwitterRequest(search: searchText!, count: 100)
+            request.fetchTweets { (newTweets) -> Void in
+                dispatch_async(dispatch_get_main_queue()){  () -> Void in
+                    if newTweets.count > 0 {
+                        self.tweets.insert(newTweets, atIndex: 0)
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refresh()
+    }
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return tweets.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tweets[section].count
+    }
+    
+    private struct Storyboard {
+        static let CellReuseIdentifier = "Tweet"
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.CellReuseIdentifier, forIndexPath: indexPath) as! TweetTableViewCell
+
+        cell.tweet = tweets[indexPath.section][indexPath.row]
+    
+        return cell
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == searchTextField {
+            textField.resignFirstResponder()
+            searchText = textField.text
+        }
+        return true
+    }
+    
+    @IBOutlet weak var searchTextField: UITextField!{
+        didSet{
+            searchTextField.delegate = self
+            searchTextField.text = searchText
+        }
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+    }
+    */
+    
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    */
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    }
+    */
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return NO if you do not want the item to be re-orderable.
+    return true
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
